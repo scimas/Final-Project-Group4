887b128ae892b570d6f4cb845f039c0573c389b3
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2019-11-11 16:59:01 -0500

    Better layers specification

--- Code/modelling.py
+++ Code/modelling.py
@@ -6,25 +6,47 @@ from torch.utils.data import Dataset
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        # 38 x 38 x 1 -> 36 x 36 x 6
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.actv1 = nn.ReLU()
-        # 36 x 36 x 6 -> 18 x 18 x 6
-        self.pool1 = nn.MaxPool2d(2)
-        # # 18 x 18 x 6 -> 16 x 16 x 12
-        self.conv2 = nn.Conv2d(6, 12, 3)
-        self.actv2 = nn.ReLU()
-        # # 16 x 16 x 12 -> 8 x 8 x 12
-        self.pool2 = nn.MaxPool2d(2)
-        # # 8 x 8 x 12 -> 26
-        self.fc = nn.Linear(8 * 8 * 12, 26)
+        self.layers = nn.ModuleDict()
+        self.layers["conv1"] = nn.ModuleList(
+            [
+                # 38 x 38 x 1 -> 36 x 36 x 12
+                nn.Conv2d(1, 16, 3),
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                # 36 x 36 x 16 -> 18 x 18 x 16
+                nn.MaxPool2d(2),
+            ]
+        )
+        self.layers["conv2"] = nn.ModuleList(
+            [
+                # 18 x 18 x 16 -> 16 x 16 x 8
+                nn.Conv2d(16, 8, 3),
+                nn.BatchNorm2d(8),
+                nn.ReLU(),
+                # 16 x 16 x 8 -> 8 x 8 x 8
+                nn.MaxPool2d(2),
+            ]
+        )
+        self.layers["fc1"] = nn.ModuleList(
+            [
+                nn.Flatten(),
+                # 8 x 8 x 8 -> 128
+                nn.Linear(8 * 8 * 8, 128),
+                nn.ReLU(),
+            ]
+        )
+        self.layers["fc2"] = nn.ModuleList(
+            [
+                # 128 -> 26
+                nn.Linear(128, 26),
+            ]
+        )
     
 
     def forward(self, X):
-        X = self.pool1(self.actv1(self.conv1(X)))
-        X = self.pool2(self.actv2(self.conv2(X)))
-        X = X.view(-1, 8 * 8 * 12)
-        X = self.fc(X)
+        for name, layerlist in self.layers.items():
+            for layer in layerlist:
+                X = layer(X)
 
         return X
 
