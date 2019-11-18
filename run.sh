136a756adcb385cc38d9cb8a54256f9b670632e5
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2019-11-17 19:43:23 -0500

    Better formatting

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ "$1" = "train" ]
-then
+if [ "$1" = "train" ]; then
     python Code/train.py
-elif [ "$1" = "evaluate" ]
-then
+elif [ "$1" = "evaluate" ]; then
     python Code/evaluate.py
-elif [ "$1" = "help" ]
-then
+elif [ "$1" = "predict" ]; then
+    python Code/predict.py
+elif [ "$1" = "help" ]; then
     echo "Usage:"
     echo "./run.sh train     # To train the model"
     echo "./run.sh evaluate  # To evaluate the trained model on test data"
