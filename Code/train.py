b5d7c672f695c21fdce51f03c4966be2fdae922e
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2019-11-28 11:14:57 -0500

    Learning rate scheduling

--- Code/train.py
+++ Code/train.py
@@ -12,21 +12,21 @@ from torch.utils.data import DataLoader
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Optimizer learning rate
-learning_rate = 1e-4
+learning_rate = 1e-2
 # Epochs
-epochs = 10
+epochs = 50
 # Validation loss early stopping patience
 # Number of epochs
-patience = 1
+patience = 10
 
-X_train, X_val, y_train, y_val = preprocess.load_data()
-train_data = MyDataset(X_train, y_train, preprocess.make_transform())
+X_train, X_val, y_train, y_val, ws = preprocess.load_data()
+train_data = MyDataset(X_train, y_train, preprocess.make_transform("train"))
 train_loader = DataLoader(
-    train_data, batch_size=128, shuffle=True
+    train_data, batch_size=128, shuffle=True, num_workers=8, pin_memory=True
 )
-test_data = MyDataset(X_val, y_val, preprocess.make_transform())
+test_data = MyDataset(X_val, y_val, preprocess.make_transform("eval"))
 test_loader = DataLoader(
-    test_data, batch_size=512, shuffle=False
+    test_data, batch_size=512, shuffle=False, num_workers=8
 )
 base_dir = os.getcwd()
 model_dir = os.path.join(base_dir, "Code", "model")
@@ -38,9 +38,10 @@ with open(os.path.join(model_dir, "model_specification"), "w") as ms:
 my_classifier = get_model(model_name)
 my_classifier.to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(my_classifier.parameters(), lr=learning_rate)
+optimizer = optim.SGD(my_classifier.parameters(), lr=learning_rate, momentum=0.9)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
 
-min_val_loss = 10e10
+min_val_loss = 1e10
 loss_decreased = 0
 for epoch in range(epochs):
     total_loss = 0
@@ -52,6 +53,8 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
+        print("\rBatch: {:3d}".format(i), end="")
+    print()
     print("Epoch: {:3d} Loss: {:6.4f}".format(epoch, total_loss/len(train_loader)), end=" ")
     with torch.no_grad():
         total_loss = 0
@@ -60,6 +63,7 @@ for epoch in range(epochs):
             pred = my_classifier(images.to(device))
             loss = criterion(pred, labels.to(device))
             total_loss += loss.item()
+        scheduler.step(total_loss/len(test_loader))
         print("Val loss: {:6.4f}".format(total_loss/len(test_loader)))
         if total_loss < min_val_loss:
             min_val_loss = total_loss
