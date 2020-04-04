99853d2783b51dccf846205345762d5c8cdbdac6
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2020-04-04 15:37:34 -0400

    Fix functions not exported

--- Code/juliacode/ASL.jl
+++ Code/juliacode/ASL.jl
@@ -7,7 +7,7 @@ using Flux.Optimise: update!
 using BSON: @save, @load
 using Dates: now
 
-export load_train_data, load_test_data, ResNet10, train!
+export load_train_data, load_test_data, ResNet10, train!, load_saved_model, test_model
 
 include("preprocess.jl")
 include("models.jl")
